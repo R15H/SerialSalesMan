4d3f5d5edfa1698ec67d6260a7e7a19fd81855a3
Alexandre Bento <alexandre.e.bento@tecnico.ulisboa.pt>
2023-02-23 18:09:56 +0000

    processing input (missing transforming to internal datastructures)

--- tsp.c
+++ tsp.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ERROR(message) printf(stderr, "message");
+
+// print macro for debugging when DEBUG is defined
+#ifdef DEBUG
+#define MESSAGE(message, ...) printf(stderr, message, __VA_ARGS__)
+#else
+#define MESSAGE(message, ...)
+#endif
+
+#define NR_OF_SOLUTIONS 100
+struct transversal {
+    int *path; // array of cities <-- can be as big as 2x the size of the number of cities
+    double cost;
+};
+
+struct transversal solution; // we only store on solution, the best, which is a complete transversal
+
 
 
 struct city {
     int id;
-    double x;
-    double y;
+
+    struct city *cities; // index i -> city i
+    double *cost;       // index i -> city i
 };
+
+
 
 void parse_inputs(int argc, char ** argv){
     if(argc < 3) {
@@ -17,14 +37,27 @@ void parse_inputs(int argc, char ** argv){
     };
     char *cities_file = argv[1];
     int lower_bound = atoi(argv[2]);
-    // read file line by line
 
+    FILE * cities_fp = fopen(cities_file, "r");
+    if (cities_fp == NULL) {
+        ERROR("File not found")
+        exit(-1);
+    }
+    char * buffer[1024];
+    fgets(buffer, 1024, cities_fp) == NULL;
+    int number_of_cities = atoi(strtok(buffer, " "));
+    int number_of_roads = atoi(strtok(NULL, " "));
+    while(fgets(buffer, 1024, cities_fp) == NULL) {
+        MESSAGE("Read line: %s", buffer);
 
+        int city_number = atoi(strtok(buffer, " "));
+        int city_destination = atoi(strtok(NULL, " "));
+        int city_cost = atoi(strtok(NULL, " "));
+    }
+    fclose(cities_fp);
+}
 
-
-
-
-
+void print_result() {
 
 }
 
